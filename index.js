171513ff3aa77f2080ac7928ef5977eeee1554e5
MUAZAMALI-dev <malikmuazambhutta@gmail.com>
2023-08-20 14:20:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+// function product(a:number, b:number){
+//     var result:number=a*b;
+//     return result;
+// }
+// var multiply:number=product(20,24);
+// multiply*=3;
+// console.log(multiply);
+// var sum=( c:number, d:number):number=> c+d;
+// var jama:Number=sum(23 ,67);
+// var sumResult:number=sum(13,45)
+// console.log(sumResult);
+// console.log(jama);
+// console.log(typeof sum);
+// var calculatRectangleArea= (height:number, width:number):number=>height * width;
+// var proResult:number=calculatRectangleArea(23,21);
+// var result:number=calculatRectangleArea(5,10);
+// console.log("Area of Rectangle=", proResult); 
+// console.log("Area of Rectangle=", result); 
+// var convertCelsiusToFahrenheit= (tempC:number):number=>tempC*(9/5)+32;
+// var tempatureconversion:number=convertCelsiusToFahrenheit(25);
+// console.log("convertCelsiusToFahrenheit=", tempatureconversion+"");
+for (let i = 1; i <= 10; i++) {
+    console.log("2" + "*" + i + "= " + 2 * i);
+}
